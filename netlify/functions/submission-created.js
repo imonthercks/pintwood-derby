adb4d84cf9dd55e3b7938d9575b3da9b05ba1ab5
imonthercks <imonthercks@gmail.com>
2023-09-02 10:18:21 -0500

    added new netlify function

---
+++ netlify/functions/submission-created.js
@@ -0,0 +1,54 @@
+// addToGoogleSheet.js
+
+const { google } = require('googleapis');
+
+exports.handler = async function (event, context) {
+  try {
+    // Load the credentials from your service account JSON key file
+    const serviceKeyJson = JSON.parse(process.env.GOOGLE_SERVICE_KEY_JSON);
+
+    // Set up the Google Sheets API using the JSON key
+    const sheets = google.sheets({ version: 'v4', auth: serviceKeyJson });
+
+    // Parse the incoming form data (assuming it's in JSON format)
+    const formData = JSON.parse(event.body);
+
+    // Define the spreadsheetId and range
+    const spreadsheetId = process.env.REGISTRATION_SPREADSHEET_ID;
+    const range = process.env.REGISTRATION_SHEET_NAME; // Change to your sheet's name
+
+    // Prepare the data and headers
+    const headers = Object.keys(formData); // Use form field names as headers
+    const values = [headers.map((header) => formData[header])];
+
+    // Call the Sheets API to append the data
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: `${sheetName}!A:A`, // Specify the range for the headers (e.g., column A)
+      valueInputOption: 'RAW',
+      resource: {
+        values: [headers],
+      },
+    });
+
+    // Append the actual data below the headers
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: `${sheetName}!A2`, // Start from the second row (assuming headers are in the first row)
+      valueInputOption: 'RAW',
+      resource: {
+        values,
+      },
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Data added to Google Sheet' }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error adding data to Google Sheet' }),
+    };
+  }
+};
