bb86ebc690559f9bc489e7be7f01f2c4a4cdc791
imonthercks <imonthercks@gmail.com>
2023-09-03 09:57:40 -0500

    debugging

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,25 +1,23 @@
 // addToGoogleSheet.js
 
 const { google } = require('googleapis');
+const process = require('process')
 
 exports.handler = async function (event, context) {
   try {
-    // Load the credentials from your service account JSON key file
-    const serviceKeyJson = JSON.parse(process.env.GOOGLE_SERVICE_KEY_JSON);
-
+    // Load environment variables
+    const {REGISTRATION_SPREADSHEET_ID, REGISTRATION_SHEET_NAME, GOOGLE_SERVICE_KEY_JSON} = process.env;
+    
     // Set up the Google Sheets API using the JSON key
-    const sheets = google.sheets({ version: 'v4', auth: serviceKeyJson });
+    const sheets = google.sheets({ version: 'v4', auth: GOOGLE_SERVICE_KEY_JSON });
 
     // Parse the incoming form data (assuming it's in JSON format)
-    const formData = JSON.parse(event.body.payload?.data);
-    //console.log(event.body);
+    const formData = JSON.parse(event.body).payload.data;
+    console.log(JSON.stringify(formData));
 
-    // Define the spreadsheetId and range
-    const spreadsheetId = process.env.REGISTRATION_SPREADSHEET_ID;
-    const range = process.env.REGISTRATION_SHEET_NAME; // Change to your sheet's name
-
-    console.log(`spreadsheetId: ${spreadsheetId}`);
-    console.log(`range: ${range}`);
+    
+    console.log(`spreadsheetId: ${REGISTRATION_SPREADSHEET_ID}`);
+    console.log(`range: ${REGISTRATION_SHEET_NAME}`);
 
     // Prepare the data and headers
     const headers = Object.keys(formData); // Use form field names as headers
